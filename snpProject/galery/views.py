3bfa8c37885788210561939ac1a9e5bcadb2ec2e
akelaPro <ilya_chernov_2022@mail.ru>
2024-11-29 17:41:30 +0000

    update model

--- snpProject/galery/views.py
+++ snpProject/galery/views.py
@@ -1,3 +1,39 @@
+import os
 from django.shortcuts import render
 
+from . models import Photo
+
+from . forms import UploadFileForm
+
 # Create your views here.
+
+
+def index(request):
+    photos = Photo.objects.all()  # Получаем все объекты Photo
+    data = {'photos': photos}  # Создаем словарь с ключом 'photos'
+    return render(request, 'galery/index.html', data)
+
+
+
+def handle_uploaded_file(f):
+    upload_dir = 'uploads'
+    
+    # Проверяем, существует ли директория, если нет - создаем
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+
+    # Сохраняем файл
+    with open(os.path.join(upload_dir, f.name), "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+
+def about(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(form.cleaned_data['file'])
+    else:
+        form = UploadFileForm()
+    return render(request, 'galery/about.html',
+                  {'title': 'О сайте', 'form': form})
